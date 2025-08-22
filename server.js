a9f40f51e8defa4b3183f0ce8b16ca866d0eb34c
Aon Mao <96533871+AonMao888@users.noreply.github.com>
2025-08-22 23:46:44 +0630

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var admin = require('firebase-admin');
 const cors = require('cors');
-var serviceAccount = require('./onway.json')
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
@@ -10,8 +10,22 @@ app.use(cors({
     origin: '*'
 }));
 
+var cer = {
+  "type": "service_account",
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.CLIENT_CERT,
+  "universe_domain": "googleapis.com"
+}
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(cer)
 });
 const db = admin.firestore();
 
